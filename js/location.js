3a182e1fa0e104846b83e68760952107e481935e
Markus Neubrand <markus.neubrand@gmail.com>
2016-10-31 17:03:43 -0500

    Layout fixes + Alexandria

--- js/location.js
+++ js/location.js
@@ -1,3 +1,3 @@
-var loc = 'Fredericksburg, VA';
+var loc = 'Guatemala City, Guatemala';
 $('#current-location').html('Currently in<br/>' + loc);
 $('#current-location-small').html('Currently in ' + loc);
