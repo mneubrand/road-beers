81822f438e2b3a445135f1a114f399f8a050c895
Markus Neubrand <markus.neubrand@gmail.com>
2017-03-10 18:34:35 +1300

    Added Argentina/Chile/Cruise

--- js/location.js
+++ js/location.js
@@ -1,3 +1,3 @@
-var loc = 'Santiago, Chile';
+var loc = 'Mount Cook, New Zealand';
 $('#current-location').html('Currently in<br/>' + loc);
 $('#current-location-small').html('Currently in ' + loc);
